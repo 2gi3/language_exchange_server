0baddb75a784408bd34dfe1a7cbb1e9a107d8d74
Giuseppe Ippoito <gippolito@hotmail.co.uk>
2023-08-07 23:25:20 +0100

    Created a list of all connected sockets

--- server.ts
+++ server.ts
@@ -1,22 +1,29 @@
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
+import { instrument } from "@socket.io/admin-ui";
+
+interface SocketInfo {
+  id: string;
+}
 
 interface Message {
   id: string;
   sender: string;
   text: string;
 }
+const connectedSockets: SocketInfo[] = [];
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:19006", "exp://192.168.43.235:8081"],
+    origin: ["http://localhost:19006", "exp://192.168.43.235:8081", "https://admin.socket.io/"],
     methods: ["GET", "POST"]
   }
 });
 
 io.on("connection", (socket: Socket) => {
+  connectedSockets.push({ id: socket.id });
 
   socket.on('client-message', (newMessage, room) => {
     room === ''
@@ -33,7 +40,21 @@ io.on("connection", (socket: Socket) => {
     )
   })
 
+  io.emit('socket-list', connectedSockets);
 
+  socket.on('disconnect', () => {
+    const index = connectedSockets.findIndex((s) => s.id === socket.id);
+    if (index !== -1) {
+      connectedSockets.splice(index, 1);
+      io.emit('socket-list', connectedSockets);
+    }
+  });
+
+
+});
+
+instrument(io, {
+  auth: false,
 });
 
 
